a51b5a59e7e2d921a6a3c5412b7413fbfade4cc5
pyj <wodus331@gmail.com>
2025-12-10 19:03:58 +0900

    update: next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL('https://yt3.ggpht.com/**')],
+  },
+  experimental: {
+    globalNotFound: true,
+  },
   turbopack: {
     rules: {
       '*.svg': {
