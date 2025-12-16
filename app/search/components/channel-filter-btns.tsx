b17b9e7d6e752c981381a5f2934df4ba65a957d2
pjy <wodus331@gmail.com>
2025-12-17 04:42:39 +0900

    add: 기타 filter modal로 처리

--- app/search/components/channel-filter-btns.tsx
+++ app/search/components/channel-filter-btns.tsx
@@ -2,30 +2,43 @@
 import { SearchQuery } from '@/app/search/page';
 import { useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { Label } from '@/components/ui/label';
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+import { ListFilterIcon } from 'lucide-react';
+import { IconClose } from '@/assets/Icon-close';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Label } from '@/components/ui/label';
 
 const TAGS = ['쇼츠', '바이럴', '리뷰어', '게임', '뷰티', 'ASMR'];
 
 const UPLOAD_PERIODS = [
-  { value: 'day', label: '최근 1일' },
-  { value: 'week', label: '최근 1주' },
-  { value: 'month', label: '최근 1개월' },
-  { value: 'year', label: '최근 1년' },
+  { value: '-1', label: '전체' },
+  { value: '7d', label: '최근 7일' },
+  { value: '14d', label: '최근 14일' },
+  { value: '1m', label: '최근 1달' },
+  { value: '2m', label: '최근 2달' },
+  { value: '3m', label: '최근 3달' },
+  { value: '6m', label: '최근 6개월' },
+  { value: '1y', label: '최근 1년' },
 ];
 
 const SORT_OPTIONS = [
-  { value: 'createdAt', label: '최신순' },
-  { value: 'subscriberCount', label: '구독자순' },
-  { value: 'viewCount', label: '조회수순' },
+  { value: 'createdAt', label: '추가날짜' },
+  { value: 'subscriberCount', label: '구독자 수' },
+  { value: 'viewCount', label: '조회수' },
 ];
+
 export default function ChannelFilterBtns({ query }: { query: SearchQuery }) {
+  const [open, setOpen] = useState(false);
   const [filters, setFilters] = useState({
     uploadAt: query.uploadAt || '',
     sort: query.sort || 'createdAt',
@@ -53,21 +66,79 @@ export default function ChannelFilterBtns({ query }: { query: SearchQuery }) {
   };
 
   return (
-    <div className="flex w-[200px] justify-center gap-2">
-      <Label>정렬</Label>
-      {/*<Select*/}
-      {/*  value={filters.sort}*/}
-      {/*  onValueChange={(value) => handleFilterChange('sort', value)}*/}
-      {/*>*/}
-      {/*  <SelectTrigger>{filters.sort}</SelectTrigger>*/}
-      {/*  <SelectContent>*/}
-      {/*    {SORT_OPTIONS.map((option) => (*/}
-      {/*      <SelectItem key={option.value} value={option.value}>*/}
-      {/*        {option.label}*/}
-      {/*      </SelectItem>*/}
-      {/*    ))}*/}
-      {/*  </SelectContent>*/}
-      {/*</Select>*/}
-    </div>
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">
+          필터
+          <ListFilterIcon />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent
+        className={'flex h-[70%] max-w-[800px] flex-1 flex-col gap-2'}
+      >
+        <AlertDialogHeader>
+          <AlertDialogTitle
+            className={
+              'border-border flex items-start justify-between border-b py-4'
+            }
+          >
+            필터옵션
+            <button className={'cursor-pointer'} onClick={() => setOpen(!open)}>
+              <IconClose className={'stroke-2'} />
+            </button>
+          </AlertDialogTitle>
+          <AlertDialogDescription hidden />
+        </AlertDialogHeader>
+        <div className="scrollWidth3 relative flex min-h-[20px] flex-1 flex-col gap-4 overflow-auto">
+          <div className={'absolute h-full w-full'}>
+            <div className={'mb-8 flex w-full flex-col gap-4'}>
+              <Label className={'font-semibold'}>테이블 정렬</Label>
+              <RadioGroup
+                defaultValue={SORT_OPTIONS[0].value}
+                className={'grid-cols-3 gap-y-5 p-2'}
+              >
+                {SORT_OPTIONS.map((option, i) => (
+                  <div
+                    className="flex w-fit items-center gap-3"
+                    key={option.value}
+                  >
+                    <RadioGroupItem value={option.value} id={'sort' + i} />
+                    <Label htmlFor={'sort' + i} className={'text-sm'}>
+                      {option.label}
+                    </Label>
+                  </div>
+                ))}
+              </RadioGroup>
+            </div>
+            <div className={'mb-8 flex w-full flex-col gap-4'}>
+              <Label className={'font-semibold'}>마지막 업로드일</Label>
+              <RadioGroup
+                defaultValue={UPLOAD_PERIODS[0].value}
+                className={'grid-cols-3 gap-y-5 p-2'}
+              >
+                {UPLOAD_PERIODS.map((option, i) => (
+                  <div
+                    className="flex w-fit items-center gap-3"
+                    key={option.value}
+                  >
+                    <RadioGroupItem value={option.value} id={'sort' + i} />
+                    <Label htmlFor={'sort' + i} className={'text-sm'}>
+                      {option.label}
+                    </Label>
+                  </div>
+                ))}
+              </RadioGroup>
+            </div>
+            <div className={'flex w-full flex-col gap-4'}>
+              <Label className={'font-semibold'}>태그</Label>
+              <div className={'p-2 text-sm'}>준비중</div>
+            </div>
+          </div>
+        </div>
+        <AlertDialogFooter>
+          <AlertDialogAction>검색</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
